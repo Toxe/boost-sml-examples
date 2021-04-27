2670aedd98cf76897fb4e5ff59ac49d1aad4fb4a
Toxe <toxe@toxe.net>
2021-04-27 23:46:42 +0200

    Add guards.cpp

---
+++ src/sml/guards.cpp
@@ -0,0 +1,56 @@
+#include <cassert>
+
+#include <boost/sml.hpp>
+#include <spdlog/spdlog.h>
+
+namespace sml = boost::sml;
+
+struct state1 {};
+struct state2 {};
+struct state3 {};
+
+struct event1 {
+    int count_;
+    event1(const int count) : count_{count} {}
+};
+
+struct event2 {};
+struct event3 {};
+
+auto guard1 = [](const event1& e) { return e.count_ == 0; };
+auto guard2 = [] { return true; };
+auto guard3 = [] { return true; };
+
+auto action1 = [] {};
+auto action2 = [] {};
+auto action3 = [] {};
+
+struct simple {
+    auto operator()()
+    {
+        using namespace sml;
+
+        return make_transition_table(
+          * state<state1> + event<event1> [ guard1 ] / action1 = state<state2>,
+            state<state2> + event<event2> [ guard2 ] / action2 = state<state3>,
+            state<state3> + event<event3> [ guard3 ] / action3 = X
+        );
+    }
+};
+
+int main()
+{
+    sml::sm<simple> sm;
+
+    sm.process_event(event1{2});
+    assert(sm.is(sml::state<state1>));  // stay in state1
+    sm.process_event(event1{1});
+    assert(sm.is(sml::state<state1>));  // stay in state1
+    sm.process_event(event1{0});
+    assert(sm.is(sml::state<state2>));  // --> transition to state2
+
+    sm.process_event(event2{});
+    sm.process_event(event3{});
+
+    assert(sm.is(sml::X));
+}
