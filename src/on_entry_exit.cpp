934714ebe0adb6851ec606a967c27c1020156070
Toxe <toxe@toxe.net>
2021-04-29 20:45:59 +0200

    Add a "stay" event

--- src/on_entry_exit.cpp
+++ src/on_entry_exit.cpp
@@ -12,6 +12,7 @@ struct state3 {};
 struct event1 {};
 struct event2 {};
 struct event3 {};
+struct stay {};
 
 auto guard1 = [] { return true; };
 auto guard2 = [] { return true; };
@@ -28,6 +29,7 @@ struct state_machine {
 
         return make_transition_table(
           * state<state1> + event<event1> [ guard1 ] / action1 = state<state2>,
+            state<state2> + event<stay>                        = state<state2>,
             state<state2> + event<event2> [ guard2 ] / action2 = state<state3>,
             state<state3> + event<event3> [ guard3 ] / action3 = X,
             state<state1> + on_entry<_> / [] { spdlog::info("state1: on_entry"); },
@@ -44,8 +46,10 @@ int main()
 {
     sml::sm<state_machine> sm;
 
-    sm.process_event(event1{});
-    sm.process_event(event2{});
+    sm.process_event(event1{});  // --> state2
+    sm.process_event(stay{});    // stay in state2
+    sm.process_event(stay{});    // stay in state2
+    sm.process_event(event2{});  // --> state3
     sm.process_event(event3{});
 
     assert(sm.is(sml::X));
