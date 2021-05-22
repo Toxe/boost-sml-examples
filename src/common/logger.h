ba97faeb5a266c3ca54d9ff60fdfa48a244258e3
Toxe <toxe@toxe.net>
2021-05-22 13:25:54 +0200

    Change logger format

--- src/common/logger.h
+++ src/common/logger.h
@@ -13,20 +13,20 @@ struct my_logger {
     template <class SM, class TGuard, class TEvent>
     void log_guard(const TGuard&, const TEvent&, bool result)
     {
-        spdlog::info("({})         guard: {} {} {}", boost::sml::aux::get_type_name<SM>(), boost::sml::aux::get_type_name<TGuard>(),
+        spdlog::info("({}) guard: {} {} {}", boost::sml::aux::get_type_name<SM>(), boost::sml::aux::get_type_name<TGuard>(),
             boost::sml::aux::get_type_name<TEvent>(), (result ? "[OK]" : "[Reject]"));
     }
 
     template <class SM, class TAction, class TEvent>
     void log_action(const TAction&, const TEvent&)
     {
-        spdlog::info("({})        action: {} {}", boost::sml::aux::get_type_name<SM>(), boost::sml::aux::get_type_name<TAction>(),
+        spdlog::info("({}) action: {} {}", boost::sml::aux::get_type_name<SM>(), boost::sml::aux::get_type_name<TAction>(),
             boost::sml::aux::get_type_name<TEvent>());
     }
 
     template <class SM, class TSrcState, class TDstState>
     void log_state_change(const TSrcState& src, const TDstState& dst)
     {
-        spdlog::info("({})    transition: {} -> {}", boost::sml::aux::get_type_name<SM>(), src.c_str(), dst.c_str());
+        spdlog::info("({}) transition: {} -> {}", boost::sml::aux::get_type_name<SM>(), src.c_str(), dst.c_str());
     }
 };
